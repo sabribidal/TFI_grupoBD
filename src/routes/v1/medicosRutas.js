26260c6943e78dfcca7ef7aec08aaa548c5257c0
Santiago <santiago.emanuelr@gmail.com>
2026-06-29 18:16:44 -0300

    seguridad medicos

--- src/routes/v1/medicosRutas.js
+++ src/routes/v1/medicosRutas.js
@@ -1,13 +1,20 @@
 import { Router } from 'express';
 import MedicosController from '../../controllers/medicos.controller.js';
+import passport from '../../middlewares/passport.js';
+import { autorizar } from '../../middlewares/autorizar.js';
 
 const router = Router();
 const controller = new MedicosController();
+const auth = passport.authenticate('jwt', { session: false });
 
-router.get('/', controller.buscarTodas);
-router.get('/:id', controller.buscarPorId);
-router.post('/', controller.crear);
-router.put('/:id', controller.actualizar);
-router.delete('/:id', controller.eliminar);
+
+router.get('/', auth, autorizar('2, 3'), controller.buscarTodas);
+router.get('/:id', auth, autorizar('2, 3'), controller.buscarPorId);
+
+
+router.post('/', auth, autorizar('3'), controller.crear);
+router.put('/:id', auth, autorizar('3'), controller.actualizar);
+router.delete('/:id', auth, autorizar('3'), controller.eliminar);
+
 
 export { router };
